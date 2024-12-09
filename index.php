a66c9491748c8797ecc39f0bdd897cb6cf300bd0
Madhurjya <mbora396@gmail.com>
2024-12-09 16:24:26 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php include 'includes/header.php'; ?>
+<main>
+    <section id="sec-0">
+        <article>
+            <div class="container">
+                <h1>We are Certified Engineers</h1>
+                <h2>Construction services<br>
+                    <span>Creative AND professional</span>
+                </h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fuga debitis distinctio doloribus quis adipisci asperiores soluta quidem nemo aperiam maiores tempora, accusamus minus odit magni corporis molestias dignissimos porro?</p>
+                <a href="#">Show More</a>
+                <a href="#">View Project</a>
+            </div>
+        </article>
+    </section>
+
+    <section id="sec-1">
+        <div class="container">
+            <article>
+                <h1>Welcome to our company</h1>
+                <h2>Lorem ipsum dolor sit amet.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate deserunt magni sint mollitia similique corporis quis voluptatibus.</p>
+                <a href="#">Show More</a>
+            </article>
+            <aside><img src="Images/img1.png" alt="man"></aside>
+        </div>
+    </section>
+
+    <section id="sec-2">
+        <div class="container">
+            <h1>Why Choose Us?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ducimus, voluptatibus perferendis voluptates quidem magni illo odit velit?</p>
+            <article>
+                <figure>
+                    <div><img src="Images/ico1.png" alt=""></div>
+                    <div class="cont">
+                        <h2>We deliver quality</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </figure>
+                <figure>
+                    <div><img src="Images/ico2.png" alt=""></div>
+                    <div class="cont">
+                        <h2>Always on time</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </figure>
+                <figure>
+                    <div><img src="Images/ico3.png" alt=""></div>
+                    <div class="cont">
+                        <h2>We are passionate</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </figure>
+                <figure>
+                    <div><img src="Images/ico4.png" alt=""></div>
+                    <div class="cont">
+                        <h2>Professional services</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </figure>
+            </article>
+        </div>
+    </section>
+
+    <section id="sec-3">
+        <div class="container">
+            <h1>Our Services</h1>
+            <div class="cont">
+                <div class="card border-0">
+                    <div>
+                        <img src="Images/card1.jpg" class="card-img-top" alt="...">
+                        <h5 class="caption">Construction Management</h5>
+                    </div>
+                    <div class="card-body px-0 pt-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <a href="#">Show More</a>
+                    </div>
+                </div>
+                <div class="card border-0">
+                    <div>
+                        <img src="Images/card2.jpg" class="card-img-top" alt="...">
+                        <h5 class="caption">Renovation</h5>
+                    </div>
+                    <div class="card-body px-0 pt-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <a href="#">Show More</a>
+                    </div>
+                </div>
+                <div class="card border-0">
+                    <div>
+                        <img src="Images/card3.jpg" class="card-img-top" alt="...">
+                        <h5 class="caption">Architecture</h5>
+                    </div>
+                    <div class="card-body px-0 pt-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <a href="#">Show More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="sec-4">
+        <div class="container">
+            <h1>Made with Love</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class="cont">
+                <img class="wow animated fadeIn slow" src="Images/gallery1.jpg" alt="pic">
+                <aside>
+                    <img src="Images/gallery3.jpg" alt="pic" class="wow animated flipInX delay-1s">
+                    <img src="Images/gallery2.jpg" alt="pic" class="wow animated flipInX delay-1s slower">
+                </aside>    
+            </div>
+        </div>
+    </section>
+</main>
+<?php include 'includes/footer.php'; ?>
